52ef4a449f95788e9446b023d65cf3ba4780e1f5
HamidrezaY81 <Hamidreza.y8105@gmail.com>
2025-01-05 14:22:14 +0000

    Deploying to gh-pages from @ HamidrezaY81/HamidrezaY81.github.io@dd13ce1c2fb4f694db2712a3678f3a42b537c254 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,7 +492,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%48%61%6D%69%64%72%65%7A%61.%79%38%31%30%35%31@%67%61%6D%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/https://github.com/HamidrezaY81", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -514,6 +521,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-telegram',
+        title: 'telegram',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://telegram.me/Hamidreza274", "_blank");
         },
       },{
         id: 'social-custom_social',
